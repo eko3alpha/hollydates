75aa4ec87a9d80eef9ff4f80858fc5f1c818ac66
Angel Alvarado <eko3alpha@gmail.com>
2020-09-03 21:32:46 -0400

    initial commit

---
+++ HollyDates.js
@@ -0,0 +1,218 @@
+(function(window){
+
+    window.HollyDates = function() {
+
+        var DAYS_IN_WEEK = 7;
+
+        var SUN = 1,
+            MON = 2,
+            TUE = 3,
+            WED = 4,
+            THU = 5,
+            FRI = 6,
+            SAT = 7;
+
+        var JAN = 1,
+            FEB = 2,
+            MAR = 3,
+            APR = 4,
+            MAY = 5,
+            JUN = 6,
+            JUL = 7,
+            AUG = 8,
+            SEP = 9,
+            OCT = 10,
+            NOV = 11,
+            DEC = 12;
+
+        var FIRST  = 1,
+            SECOND = 2,
+            THIRD  = 3,
+            FOURTH = 4,
+            LAST   = -1;
+
+        var CUSTOM_DATES = {
+            "New Year's Day": [JAN, 1],
+            "Washington's Birthday": [FEB, 17],
+            "Independence Day": [JUL, 4],
+            "Veterans Day": [NOV, 11],
+            "Christmas": [DEC, 25],
+        }
+
+        var HOLIDAYS = {
+            "Martin Luther King, Jr. Day": [THIRD, MON, JAN],
+            "Memorial Day": [LAST, MON, MAY],
+            "Labor Day": [FIRST, MON, SEP],
+            "Columbus Day": [SECOND, MON, OCT],
+            "Daylight Savings Time Ends": [FIRST, SUN, NOV],
+            "Thanksgiving Day": [LAST, THU, NOV]
+        }
+
+        var reset = function()
+        {
+            CUSTOM_DATES = {};
+            HOLIDAYS = {};
+        }
+
+        var addByOccurance = function(holiday, occurance, weekDay, month)
+        {
+            HOLIDAYS[holiday] = [occurance, weekDay, month];
+            return HOLIDAYS;
+        }
+
+        var addByDate = function(customDate, month, date)
+        {
+            CUSTOM_DATES[customDate] = [month, date];
+            return CUSTOM_DATES;
+        }
+
+        var getMonthAWeekFromDate = function(date) {
+            var newDate = new Date(date);
+            newDate.setDate(date.getDate() + DAYS_IN_WEEK);
+            return newDate.getMonth() + 1;
+        }
+
+        var isDate = function(date, month, day) {
+            if (date.getMonth() != month - 1) {
+                return false;
+            }
+
+            if (date.getDate() != day) {
+                return false;
+            }
+
+            return true;
+        }
+
+        var isDay = function(date, occurance, weekDay, month) {
+
+            if (date.getMonth() != month - 1) {
+                return false;
+            }
+
+            if (date.getDay() != weekDay - 1) {
+                return false;
+            }
+
+            if (occurance == Math.ceil(date.getDate() / DAYS_IN_WEEK)) {
+                return true;
+            }
+
+            if (occurance != LAST) {
+                return false;
+            }
+
+            return month != getMonthAWeekFromDate(date);
+
+        }
+
+        var getHoliday = function(date) {
+            var results = Object.keys(HOLIDAYS).filter(function(val, index) {
+                var occurance = HOLIDAYS[val][0];
+                var weekDay = HOLIDAYS[val][1];
+                var month = HOLIDAYS[val][2];
+
+                if (isDay(date, occurance, weekDay, month)) {
+                    return true;
+                }
+
+                return false;
+            });
+
+            if (results) {
+                return results[0];
+            }
+
+            return null;
+
+        }
+
+        var getDate = function(date) {
+            var results = Object.keys(CUSTOM_DATES).filter(function(val, index) {
+                var month = CUSTOM_DATES[val][0];
+                var day = CUSTOM_DATES[val][1];
+
+                if (isDate(date, month, day)) {
+                    return true;
+                }
+
+                return false;
+            });
+
+            if (results) {
+                return results[0];
+            }
+
+            return null;
+        }
+
+        var isHoliday = function(date) {
+
+            if (getHoliday(date)) {
+                return true;
+            }
+
+            if (getDate(date)) {
+                return true;
+            }
+
+            return false;
+        }
+
+        var getHolidays = function()
+        {
+            return HOLIDAYS;
+        }
+
+        var getCustomDates = function()
+        {
+            return CUSTOM_DATES;
+        }
+
+        return {
+            'isHoliday': isHoliday,
+            'addByOccurance': addByOccurance,
+            'addByDate': addByDate,
+            'getHoliday': getHoliday,
+            'reset': reset,
+
+            'SUN': 1,
+            'MON': 2,
+            'TUE': 3,
+            'WED': 4,
+            'THU': 5,
+            'FRI': 6,
+            'SAT': 7,
+
+            'JAN': 1,
+            'FEB': 2,
+            'MAR': 3,
+            'APR': 4,
+            'MAY': 5,
+            'JUN': 6,
+            'JUL': 7,
+            'AUG': 8,
+            'SEP': 9,
+            'OCT': 0,
+            'NOV': 11,
+            'DEC': 12,
+
+            'FIRST': 1,
+            'SECOND': 2,
+            'THIRD': 3,
+            'FOURTH': 4,
+            'LAST': -1,
+
+            '_': {
+                'getHolidays': getHolidays,
+                'getCustomDates': getCustomDates,
+                'getDate': getDate,
+                'isDay': isDay,
+                'isDate': isDate,
+                'getMonthAWeekFromDate': getMonthAWeekFromDate
+            }
+        }
+    }
+
+
+})(window);
---
+++ test.js
@@ -0,0 +1,235 @@
+var h = HollyDates();
+
+var module = QUnit.module;
+var test = QUnit.test;
+
+module('getMonthAWeekFromDate', function () {
+    test('check month of following week', function (assert) {
+        assert.equal(
+            h._.getMonthAWeekFromDate(new Date('12/31/2020')),
+            h.JAN,
+            'a week after Dec 31 2020 is Jan'
+        );
+
+        assert.equal(
+            h._.getMonthAWeekFromDate(new Date('12/24/2020')),
+            h.DEC,
+            'a week after Dec 24 2020 is still Dec'
+        );
+    });
+});
+
+module('isDay', function () {
+    test('sunday in month', function (assert) {
+
+        assert.true(
+            h._.isDay(new Date('7/1/2018'), h.FIRST, h.SUN, h.JUL),
+            '7/1/2018 first instance on first day of the month'
+        );
+
+        assert.true(
+            h._.isDay(new Date('1/7/2018'), h.FIRST, h.SUN, h.JAN),
+            '1/7/2018 first instance on second week'
+        );
+
+        assert.true(
+            h._.isDay(new Date('12/31/2017'), h.LAST, h.SUN, h.DEC),
+            '12/31/2017 last instance on last day of the month'
+        );
+
+    });
+
+    test('friday in month', function (assert) {
+
+        assert.true(
+            h._.isDay(new Date('12/1/2017'), h.FIRST, h.FRI, h.DEC),
+            '12/1/2017 first instance falls on first of the month'
+        );
+
+        assert.true(
+            h._.isDay(new Date('9/7/2018'), h.FIRST, h.FRI, h.SEP),
+            '9/7/2018 first instance on second week'
+        );
+
+        assert.true(
+            h._.isDay(new Date('11/30/2018'), h.LAST, h.FRI, h.NOV),
+            '11/30/2018 last instance on last day of the month'
+        );
+
+    });
+});
+
+module('getHoliday', function () {
+    test('Martin Luther King, Jr. Day', function (assert) {
+
+        var HOLIDAY = 'Martin Luther King, Jr. Day';
+
+        assert.equal(
+            h.getHoliday(new Date('1/18/2021')),
+            HOLIDAY,
+            '1/18/2021'
+        );
+
+        assert.equal(
+            h.getHoliday(new Date('1/15/2103')),
+            HOLIDAY,
+            '1/15/2103'
+        );
+
+        assert.equal(
+            h.getHoliday(new Date('1/20/1986')),
+            HOLIDAY,
+            '1/20/1986'
+        );
+
+    });
+
+    test('Thanksgiving Day', function (assert) {
+
+        var HOLIDAY = 'Thanksgiving Day';
+
+        assert.equal(
+            h.getHoliday(new Date('11/26/2015')),
+            HOLIDAY,
+            '11/26/2015'
+        );
+
+        assert.equal(
+            h.getHoliday(new Date('11/30/2023')),
+            HOLIDAY,
+            '11/30/2023'
+        );
+
+        assert.equal(
+            h.getHoliday(new Date('11/27/2025')),
+            HOLIDAY,
+            '11/27/2025'
+        );
+
+    });
+});
+
+module('getDate', function () {
+    test('custom dates', function (assert) {
+
+        var HOLIDAY = 'Christmas';
+
+        assert.equal(
+            h._.getDate(new Date('12/25/2021')),
+            HOLIDAY,
+            '1/25/2021 is Christmas'
+        );
+
+        var HOLIDAY = 'New Years';
+
+        assert.equal(
+            h._.getDate(new Date('1/1/2021')),
+            HOLIDAY,
+            '1/1/2021 is New Years'
+        );
+
+    });
+});
+
+
+module('isHoliday', function () {
+    test('closed for holidays', function (assert) {
+
+        assert.true(
+            h.isHoliday(new Date('9/7/2020')),
+            '9/7/2020 Labor Day'
+        );
+
+        assert.true(
+            h.isHoliday(new Date('1/16/2023')),
+            '1/16/2023 MLK'
+        );
+
+    });
+
+    test('closed for dates', function (assert) {
+
+        assert.true(
+            h.isHoliday(new Date('1/1/2020')),
+            '1/1/2020 New Years'
+        );
+
+        assert.true(
+            h.isHoliday(new Date('12/25/2023')),
+            '12/25/2023 Christmas'
+        );
+
+    });
+});
+
+module('isDate', function () {
+    test('8/16/1979', function (assert) {
+
+        assert.true(
+            h._.isDate(new Date('8/16/1979'), h.AUG, 16),
+            '8/16/1979'
+        );
+
+    });
+
+    test('8/17/1979', function (assert) {
+
+        assert.false(
+            h._.isDate(new Date('8/17/1979'), h.AUG, 16),
+            'wrong day 8/17/1979 is not 8/16'
+        );
+
+        assert.false(
+            h._.isDate(new Date('7/16/1979'), h.AUG, 16),
+            'wrong month 7/16/1979 is not 8/16'
+        );
+
+    });
+});
+
+module('addByOccurance', function () {
+    test('Last Day of School', function (assert) {
+
+        h.addByOccurance('Last Day of School', h.LAST, h.FRI, h.JUN);
+
+        assert.equal(
+            h.getHoliday(new Date('6/26/2020')),
+            'Last Day of School'
+        );
+    });
+});
+
+module('addByDate', function () {
+
+    test('My Birthday', function (assert) {
+
+        h.addByDate('My Birthday', h.AUG, 16);
+
+        assert.equal(
+            h._.getDate(new Date('8/16/2020')),
+            'My Birthday'
+        );
+    });
+});
+
+module('reset', function () {
+    test('reset', function (assert) {
+
+        var h = HollyDates();
+
+        h.addByOccurance('Last Day of School', h.LAST, h.FRI, h.JUN);
+        h.reset();
+
+        assert.deepEqual(
+            h._.getHolidays(),
+            {},
+            'reset should clear all holidays'
+        );
+
+        assert.deepEqual(
+            h._.getCustomDates(),
+            {},
+            'reset should clear all custom dates'
+        );
+    });
+});
